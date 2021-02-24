d87a3a4e17bfc3b385c1ecb02d1a5d2b0cbeea60
5ql <76627407+5ql@users.noreply.github.com>
2021-02-24 20:17:20 +0100

    Create Steins-obfu.py

---
+++ Steins-obfu.py
@@ -0,0 +1,15 @@
+import sys
+import os
+
+def main():
+	try:    
+		if len(sys.argv) < 2:
+			print("Usage: python3 {} `your input`".format(os.path.basename(__file__)))
+		else:
+		    sc = "\\x" + "\\x".join("{0:x}".format(ord(c)) for c in sys.argv[1])
+		    shellcode =f'"{sc}"'
+		    print("[*] Here is the encoded string:\n {}".format(shellcode))
+	except:
+		pass
+main()
+
