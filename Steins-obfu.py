627e1e5b3ccdd492b4fda60481e84620f603f423
5ql <76627407+5ql@users.noreply.github.com>
2021-08-10 03:15:18 +0100

    Update Steins-obfu.py

--- Steins-obfu.py
+++ Steins-obfu.py
@@ -2,14 +2,14 @@ import sys
 import os
 
 def main():
-	try:    
-		if len(sys.argv) < 2:
-			print("Usage: python3 {} `your input`".format(os.path.basename(__file__)))
-		else:
-		    sc = "\\x" + "\\x".join("{0:x}".format(ord(c)) for c in sys.argv[1])
-		    shellcode =f'"{sc}"'
-		    print("[*] Here is the encoded string:\n {}".format(shellcode))
-	except:
-		pass
-main()
+  
+	if len(sys.argv) < 2:
+		print("Usage: python3 {} `your input`".format(os.path.basename(__file__)))
+	else:
+       	        sc = "\\x" + "\\x".join("{0:x}".format(ord(c)) for c in sys.argv[1])
+	        shellcode =f'"{sc}"'
+		print("[*] Here is the encoded string:\n {}".format(shellcode))
+
+if __name__ == “main”:
+	main()
 
